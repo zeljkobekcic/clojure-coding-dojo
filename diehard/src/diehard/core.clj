7ca3e3a31498b9f5d95cdcd507bbd15cc9bed10f
Zeljko Bekcic <bekciczeljko@gmail.com>
2017-04-09 17:33:40 +0200

    Finished now this kata. Now it will change the cart if there is a group of 5 and 3 to two groups of 4, because then you will get a better price.

--- diehard/src/diehard/core.clj
+++ diehard/src/diehard/core.clj
@@ -1,9 +1,16 @@
 (ns diehard.core
 	(:require  [clojure.spec :as spec]))
 
+(def sale {0 1.0, 
+           1 1.0, 
+           2 0.95, 
+           3 0.9, 
+           4 0.8, 
+           5 0.75})
+
 (defn best-group
   [cart]
-  (count (filter #(not (zero? %)) (map cart (keys cart)))))
+  (count (filter pos? (vals cart))))
 
 (defn subtract-once-from-cart
   [cart]  
@@ -14,17 +21,31 @@
         cart))
 
 (defn calculate-price
-  [amount sale] 
-  (* (* 8 amount) sale))
+  [amount] 
+  (* 8 amount (get sale amount)))
 
 (defn calculate-best-sale-groups
   [cart]
-  (let [newCart (subtract-once-from-cart cart)
-        amount (best-group cart)
-        price (calculate-price amount (- 1 (* amount 0.05)))]
-      (if (= newCart cart) 
-        '() 
-        (conj (calculate-best-sale-groups newCart) price ))))
+  (loop [newCart cart 
+         amount  '()]
+    (if (some pos? (vals newCart)) 
+      (recur 
+        (subtract-once-from-cart newCart) 
+        (conj amount (best-group newCart)))
+      amount
+      )))
+
+(defn optimize-sale-groups
+  [sale-groups]
+  (let [amount5 (count (filter (partial = 5) sale-groups))
+        amount3 (count (filter (partial = 3) sale-groups))
+        amount4 (* 2 (min amount3 amount5))]
+        (into  
+          (into 
+            (remove #(or (= 3 %) (= 5 %)) sale-groups)
+            (repeat (Math/abs (- amount3 amount5)) 
+                    (if (> amount5 amount3) 5 3)))
+          (repeat amount4 4))))
 
 (defn calculate-price-for-cart
   [cart]
--- diehard/test/diehard/core_test.clj
+++ diehard/test/diehard/core_test.clj
@@ -17,6 +17,43 @@
 (def penta-cart-0 {:a 2 :b 3 :c 2 :d 2 :e 3})
 (def penta-cart-1 {:a 1 :b 1 :c 3 :d 3 :e 3})
 
+(deftest test-sale-group-optimization
+  (is (= {1 1} 
+        (frequencies (optimize-sale-groups (calculate-best-sale-groups single-cart)))))
+  (is (= {2 2} 
+        (frequencies (optimize-sale-groups (calculate-best-sale-groups double-cart)))))
+  (is (= {2 1 3 1}
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups tripple-cart-0)))))
+  (is (= {3 2} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups tripple-cart-1)))))
+  (is (= {1 2 3 1} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups tripple-cart-2)))))
+  (is (= {2 3} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups tripple-cart-3)))))
+  (is (= {4 1} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups quad-cart-0)))))
+  (is (= {1 1 2 1 4 2} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups quad-cart-1)))))
+  (is (= {1 1 2 1 4 1} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups quad-cart-2)))))
+  (is (= {2 1 5 2}
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups penta-cart-0)))))
+  (is (= {3 1 4 2} 
+         (frequencies (optimize-sale-groups (calculate-best-sale-groups penta-cart-1))))))
+
+(deftest test-best-sale-groups
+  (is (= '(1) (calculate-best-sale-groups single-cart)))
+  (is (= '(2 2) (calculate-best-sale-groups double-cart)))
+  (is (= '(2 3) (calculate-best-sale-groups tripple-cart-0)))
+  (is (= '(3 3) (calculate-best-sale-groups tripple-cart-1)))
+  (is (= '(1 1 3) (calculate-best-sale-groups tripple-cart-2)))
+  (is (= '(2 2 2) (calculate-best-sale-groups tripple-cart-3)))
+  (is (= '(4) (calculate-best-sale-groups quad-cart-0)))
+  (is (= '(1 2 4 4) (calculate-best-sale-groups quad-cart-1)))
+  (is (= '(1 2 4) (calculate-best-sale-groups quad-cart-2)))
+  (is (= '(2 5 5) (calculate-best-sale-groups penta-cart-0)))
+  (is (= '(3 3 5) (calculate-best-sale-groups penta-cart-1))))
+
 (deftest test-subtract-once-from-cart
   (is (= {:a 0} (subtract-once-from-cart single-cart)))
   (is (= {:a 1 :b 1} (subtract-once-from-cart double-cart)))
@@ -24,16 +61,16 @@
   (is (= {:a 1 :b 1 :c 1} (subtract-once-from-cart tripple-cart-1)))
   (is (= {:a 2 :b 0 :c 0} (subtract-once-from-cart tripple-cart-2)))
   (is (= {:a 2 :b 0 :c 2} (subtract-once-from-cart tripple-cart-3)))
-  (is (= {:a 0 :b 0 :c 0 :d 0} (subtract-once-from-cart quad-cart-0))) 
-  (is (= {:a 1 :b 1 :c 3 :d 2} (subtract-once-from-cart quad-cart-1))) 
+  (is (= {:a 0 :b 0 :c 0 :d 0} (subtract-once-from-cart quad-cart-0)))
+  (is (= {:a 1 :b 1 :c 3 :d 2} (subtract-once-from-cart quad-cart-1)))
   (is (= {:b 0 :c 2 :d 1 :e 0} (subtract-once-from-cart quad-cart-2)))
-  (is (= {:a 1 :b 2 :c 1 :d 1 :e 2} (subtract-once-from-cart penta-cart-0))) 
+  (is (= {:a 1 :b 2 :c 1 :d 1 :e 2} (subtract-once-from-cart penta-cart-0)))
   (is (= {:a 0 :b 0 :c 2 :d 2 :e 2} (subtract-once-from-cart penta-cart-1))))
 
 (deftest test-best-group
   (is (= 1 (best-group single-cart)))
   (is (= 2 (best-group double-cart)))
-  (is (= 3 (best-group tripple-cart-0))) 
+  (is (= 3 (best-group tripple-cart-0)))
   (is (= 3 (best-group tripple-cart-1)))
   (is (= 3 (best-group tripple-cart-2)))
   (is (= 2 (best-group tripple-cart-3)))
